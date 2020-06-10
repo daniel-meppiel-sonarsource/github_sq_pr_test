016f9e8c944549866a3ec92d0fe53babfc16239f
Daniel Meppiel <daniel.meppiel@sonarsource.com>
2020-06-10 13:52:04 +0200

    Added a Python code coverage example

--- github_sq_pr_test/app.py
+++
@@ -1,4 +0,0 @@
-def hello_world():
-    a = 42
-    b = 5
-    return a + b
--- github_sq_pr_test/views.py
+++
@@ -1,9 +0,0 @@
-
-
-def buggyMethod(a, b, c):
-    b = 42
-    a = b
-    for e in a:
-        a += 1
-    return None
-    c = a
