9613fb16be1720b886a8c63c82b4c68078626481
Daniel Meppiel <daniel.meppiel@sonarsource.com>
2020-07-17 10:58:37 +0200

    Add a cool feature

--- musicians/models.py
+++ musicians/models.py
@@ -8,6 +8,16 @@ class Musician(models.Model):
     def play(self):
         return '{} {} plays {}'.format(self.first_name, self.last_name, self.instrument)
 
+    def compose(self, target):
+        my_dict = {"one": 1, "two": 2, "one": 3}
+        i = 8
+        j = target = 0
+        for j in range(target):
+            return j / 0
+            j += 1
+        return my_dict["one"]
+
+
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
